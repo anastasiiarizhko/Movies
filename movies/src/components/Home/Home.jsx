5284379427a4558afcb277b9ccff42dd3970726e
anastasiiarizhko <rizhkoanastasiia@gmail.com>
2024-12-16 08:17:27 +0200

    Movies info

--- movies/src/components/Home/Home.jsx
+++ movies/src/components/Home/Home.jsx
@@ -4,11 +4,20 @@ import './home.css';
 import Footer from './Footer';
 import Main from './Main';
 import ListMovie from './ListMovie';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieDetail from '../MoviePage/MovieDetail';
 import GenreMovies from './GenreMovies';
 import SearchResults from './SearchResults';
 import { useSearch } from '../../context/ContextSearch';
+
+function NotFound() {
+  return (
+    <div className='massage-notfound'>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
 
 function Home() {
     const { movies } = useSearch();
@@ -28,6 +37,7 @@ function Home() {
               <Route path="/movie/:movieId" element={<MovieDetail />} />
               <Route path="/genre/:genreId" element={<GenreMovies />} />
               <Route path="/search" element={<SearchResults />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
 
             <Footer></Footer>
